83e746e2f3112d1de0d6ffe1089c7f7a000ffe4e
Moritz Basel <moritz.basel@uni-wuerzburg.de>
2017-12-13 13:54:22 +0100

    typo in line 4 Server -> server

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-/bin/cp /var/git/Mitfahrgelegenheit/Interne_server.py /var/WebSrv/Interne_Server.py
+/bin/cp /var/git/Mitfahrgelegenheit/Interne_server.py /var/WebSrv/Interne_server.py
 /bin/cp /var/git/Mitfahrgelegenheit/Mitfahrgelegenheit_nginx.conf /var/WebSrv/Mitfahrgelegenheit_nginx.conf
 /bin/cp /var/git/Mitfahrgelegenheit/uwsgi.ini /var/WebSrv/uwsgi.ini
 /bin/cp /var/git/Mitfahrgelegenheit/wsgi.py /var/WebSrv/wsgi.py
