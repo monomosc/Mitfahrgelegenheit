f1a5e68fe65e7f8c13192b395974d1c152130e26
Moritz Basel <moritz.basel@uni-wuerzburg.de>
2017-12-13 13:26:32 +0100

    Work on allowing all local users (non-root) to execute deployment (work bcs restarting nginx or uwsgi requires sudo)

--- deployment.sh
+++ deployment.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-./git pull
 
-/bin/cp ./Interne_Server.py /var/WebSrv/Interne_Server.py
-/bin/cp ./Mitfahrgelegenheit_nginx.conf /var/WebSrv/Mitfahrgelegenheit_nginx.conf
-/bin/cp ./uwsgi.ini /var/WebSrv/uwsgi.ini
-/bin/cp ./wsgi.py /var/WebSrv/wsgi.py
 
-sudo systemctl restart uwsgi_Mitfahrgelegenheit
-sudo systemctl restart nginx
+/bin/cp /var/git/Mitfahrgelegenheit/Interne_Server.py /var/WebSrv/Interne_Server.py
+/bin/cp /var/git/Mitfahrgelegenheit/Mitfahrgelegenheit_nginx.conf /var/WebSrv/Mitfahrgelegenheit_nginx.conf
+/bin/cp /var/git/Mitfahrgelegenheit/uwsgi.ini /var/WebSrv/uwsgi.ini
+/bin/cp /var/git/Mitfahrgelegenheit/wsgi.py /var/WebSrv/wsgi.py
+
+
+
+systemctl daemon-reload
+systemctl restart uwsgi_Mitfahrgelegenheit
+systemctl restart nginx
