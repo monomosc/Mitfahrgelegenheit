3595b49e0d90477f85efb0384c225f616de209df
Moritz Basel <moritz.basel@stud-mail.uni-wuerzburg.de>
2018-01-12 01:52:39 +0100

    Version has to be capitalized

--- deployment.sh
+++ deployment.sh
@@ -6,7 +6,7 @@
 /bin/cp /var/git/Mitfahrgelegenheit/uwsgi.ini /var/WebSrv/uwsgi.ini
 /bin/cp /var/git/Mitfahrgelegenheit/wsgi.py /var/WebSrv/wsgi.py
 /bin/cp /var/git/Mitfahrgelegenheit/Interne_Entities.py /var/WebSrv/Interne_Entities.py
-/bin/cp /var/git/Mitfahrgelegenheit/version.conf /var/WebSrv/version.conf
+/bin/cp /var/git/Mitfahrgelegenheit/Version.conf /var/WebSrv/Version.conf
 
 sudo systemctl daemon-reload
 touch /tmp/uwsgi_reload
