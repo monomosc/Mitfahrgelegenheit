d5973d108ebb49992c05b3974293ee395162c75b
Moritz Basel <moritz.basel@uni-wuerzburg.de>
2018-01-26 13:34:41 +0100

    Set Repeat time to Weekly/Daily from weekly/Daily

--- Interne_server.py
+++ Interne_server.py
@@ -1314,7 +1314,7 @@ def terminateAppointment(appointmentID):
             logger.info(str(recipientList))
     
 
-        if thisappointment.repeatTime is "daily":
+        if thisappointment.repeatTime is "Daily":
             logger.info('Creating new Appointment 1 day after Appointment #' + str(thisappointment.id))
             try:
                 newappointment = Appointment(startLocation=thisappointment.startLocation,
@@ -1333,7 +1333,7 @@ def terminateAppointment(appointmentID):
             except:
                 logger.exception('Could not create repeated Appointment')
 
-        if thisappointment.repeatTime == "weekly":
+        if thisappointment.repeatTime == "Weekly":
             logger.info('Creating new Appointment 1 week after Appointment #' + str(thisappointment.id))
             try:
                 newappointment = Appointment(startLocation=thisappointment.startLocation,
